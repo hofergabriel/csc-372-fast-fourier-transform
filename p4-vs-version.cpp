10203caf9d08d45f08adf494789e4c2dc18eabe5
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-11-19 05:15:07 -0700

    Update p4-vs-version.cpp

--- p4-vs-version.cpp
+++ p4-vs-version.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <iomanip>
 #include <intrin.h>
+#include <fstream>
 using namespace std;
 using namespace complex_literals;
 using cd = complex<double>;
@@ -47,23 +48,25 @@ void print_result(vector<cd>& a) {
     if (a.size() <= 8) for (auto el : a) cout << el << endl;
     else {
         vector<pair<int, cd>> indexed(a.size());
-        for (int i = 0; i < a.size(); i++) indexed[i] = { i,a[i] };
+        for (int i = 0; i < (int)a.size(); i++) indexed[i] = { i,a[i] };
         sort(indexed.begin(), indexed.end(), cmp);
         for (int i = 0; i < 7; i++) cout << setw(10) << left << indexed[i].first << indexed[i].second << endl;
     }
 }
 
-vector<cd> read_input() {
+vector<cd> read_input(string filename) {
+    ifstream fin(filename, ifstream::in);
+    if (!fin.is_open()) cout << "file is NOT open" << endl;
     double e1; cd e2;
     vector<cd> v;
-    while (cin >> e1) v.push_back(e2 = e1 + 0i);
+    while (fin >> e1) v.push_back(e2 = e1 + 0i);
     reverse(v.begin(), v.end()); // reverse the input
     while (__popcnt(v.size()) > 1) v.push_back(0);
     return v;
 }
 
-int main() {
-    vector<cd> v = read_input();
+int main(int argc, char * argv[]) {
+    vector<cd> v = read_input(argv[1]);
     fft(v);
     print_result(v);
     return 0;
